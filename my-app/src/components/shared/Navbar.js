3ca223ca2d9964449f77cea8c3df5d86bfb5ccfc
Manohar-D-S <manoharcds2918@gmail.com>
2025-01-05 02:38:19 +0530

    small tweaks

--- my-app/src/components/shared/Navbar.js
+++ my-app/src/components/shared/Navbar.js
@@ -1,13 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import logo from "./name.png";
 
 const Navbar = ({ handleLogout, navItems }) => {
+  const navigate = useNavigate();
+  const userType = sessionStorage.getItem("userType");
+
+  const handleBrandClick = () => {
+    if (userType === "doctor") {
+      navigate("/physiotherapist");
+    } else if (userType === "patient") {
+      navigate("/patient");
+    } else {
+      navigate("/login");
+    }
+  };
+
   return (
     <nav className="navbar navbar-expand-lg bg-secondary fixed-top" id="mainNav">
       <div className="container-fluid">
-        <Link className="navbar-brand js-scroll-trigger" to="/">
-          PhysioFit
-        </Link>
+        <button 
+          className="navbar-brand js-scroll-trigger"
+          onClick={handleBrandClick}
+          style={{ background: 'none', border: 'none', padding: 0 }}
+        >
+          <img 
+            src={logo} 
+            alt="PhysioFit Logo" 
+            height="40" 
+            style={{ maxHeight: '40px', width: 'auto' }}
+          />
+        </button>
         <button
           className="navbar-toggler navbar-toggler-right font-weight-bold bg-primary text-white rounded"
           type="button"
